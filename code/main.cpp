201ceb3b2446ac8ce0445d56b6489cb7cfe09886
Ashmit <codeatashmit@gmail.com>
2024-07-25 10:48:17 +0530

    convex hull representation

--- code/main.cpp
+++ code/main.cpp
@@ -82,5 +82,14 @@ int main() {
 
     // Printing the DCEL structure
     dcel.printDCEL();*/
-    return 0;
+    // CONVEX HULL 
+    /*std::vector<Point> points = {
+        {0, 3}, {2, 2}, {1, 1}, {2, 1}, {3, 0}, {0, 0}, {3, 3}
+    };
+
+    std::vector<Point> hull = ConvexHull::giftWrapping(points);
+
+    std::cout << "The points in the convex hull are:\n";
+    ConvexHull::printPoints(hull);
+    return 0;*/
 }
